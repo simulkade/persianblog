38e4ad3cdb167d8bb26af7942122505786ef3e43
AAE <e.eftekhari@gmail.com>
2015-04-10 22:02:15 +0200

    number of posts to three

--- conf.py
+++ conf.py
@@ -781,7 +781,7 @@ COMMENT_SYSTEM_ID = "aidadaddy"
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 3
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
